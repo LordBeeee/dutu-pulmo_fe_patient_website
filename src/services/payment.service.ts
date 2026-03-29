11126b10eaa2774958e26605c35d843d6151a3e7
Quoc Tuan <quoc.tuan@gmail.com>
2026-03-29 20:51:29 +0700

    hoan thanh payment

---
+++ src/services/payment.service.ts
@@ -0,0 +1,15 @@
+﻿import { api } from '@/services/api';
+
+export type PaymentCreateResponse = {
+  checkoutUrl?: string;
+};
+
+export const paymentService = {
+  createPayment: async (appointmentId: string) => {
+    const { data } = await api.post<PaymentCreateResponse>('/payment/create', {
+      appointmentId,
+    });
+    return data;
+  },
+};
+
